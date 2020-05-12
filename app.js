d19dae2cc1099e4104476c63ca564a09b87e5c9e
Quinton Fults <coding.banjo@gmail.com>
2020-05-12 13:04:17 -0700

    code

--- app.js
+++ app.js
@@ -1,13 +1,58 @@
-const inquirer = require('inquirer')
+const { prompt } = require('inquirer')
 
-inquirer.prompt([
-  {
+
+const info = ['name', 'age', 'email', 'username', 'password', 'address', 'phone', 'ssn']
+
+let questions = []
+for (let i = 0; i < info.length; i++) {
+  questions.push({
     type: 'input',
-    name: 'example',
-    message: 'Type anything:'
-  }
-])
+    name: info[i],
+    message: `What is your ${info[i]}?`
+  })
+}
+
+prompt(questions)
   .then(data => {
-    console.log(data.example)
+    console.log(data)
   })
   .catch(err => console.log(err))
+
+// for (let i = 0; i < info.length; i++) {
+//   prompt({
+//     type: 'input',
+//     name: info[i],
+//     message: `What is your ${info[i]}?`
+//   })
+//     .then(data => {
+//       console.log(data)
+//     })
+//     .catch(err => console.log(err))
+// }
+
+// inquirer.prompt([
+//   {
+//     type: 'input',
+//     name: 'name',
+//     message: 'What is your name?'
+//   },
+//   {
+//     type: 'input',
+//     name: 'age',
+//     message: 'How old are you?'
+//   },
+//   {
+//     type: 'input',
+//     name: 'email',
+//     message: 'What is your email?'
+//   },
+//   {
+//     type: 'input',
+//     name: 'address',
+//     message: 'What is your address?'
+//   }
+// ])
+//   .then(data => {
+//     console.log(data)
+//   })
+//   .catch(err => console.log(err))
