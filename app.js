2887243d824b4e34724ea6fb67b68c912075772a
Quinton Fults <coding.banjo@gmail.com>
2020-05-12 11:22:49 -0700

    code

---
+++ app.js
@@ -0,0 +1,13 @@
+const inquirer = require('inquirer')
+
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'example',
+    message: 'Type anything:'
+  }
+])
+  .then(data => {
+    console.log(data.example)
+  })
+  .catch(err => console.log(err))
